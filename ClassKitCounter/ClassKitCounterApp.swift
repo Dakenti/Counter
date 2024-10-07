4522149aff6eca9273a3bbc46d8bc9cd1dd4f95d
Darkhan Serkeshev <darkhan.serkeshev@waveaccess.ru>
2024-10-07 18:51:25 +0500

    initial commit

---
+++ ClassKitCounter/ClassKitCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassKitCounterApp.swift
+//  ClassKitCounter
+//
+//  Created by Darkhan Serkeshev on 07.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ClassKitCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
