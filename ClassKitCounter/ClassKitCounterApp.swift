4cd3ab757ec3bbd7cd1086c49d3b460ba9e7eec5
Darkhan Serkeshev <darkhan.serkeshev@waveaccess.ru>
2024-10-07 19:35:06 +0500

    completed minimum requirements

--- ClassKitCounter/ClassKitCounterApp.swift
+++ ClassKitCounter/ClassKitCounterApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct ClassKitCounterApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      CounterView()
     }
+  }
 }
--- ClassKitCounter/ContentView.swift
+++ ClassKitCounter/ContentView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  CounterView.swift
 //  ClassKitCounter
 //
 //  Created by Darkhan Serkeshev on 07.10.2024.
@@ -7,18 +7,70 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+// MARK: - CounterView
+
+public struct CounterView: View {
+  @State
+  private var count = 0
+  
+  public var body: some View {
+    content
+  }
 }
 
-#Preview {
-    ContentView()
+// MARK: - Content
+
+extension CounterView {
+  private var content: some View {
+    VStack {
+      counterText
+      buttons
+    }
+  }
+  
+  private var counterText: some View {
+    Text("\(count)")
+      .font(.system(size: 80))
+      .foregroundStyle(.gray)
+  }
+  
+  private var buttons: some View {
+    HStack(alignment: .center, spacing: 16) {
+      resetButton
+      incrementButton
+    }
+  }
+  
+  private var resetButton: some View {
+    Button(action: {
+      count = 0
+    }) {
+      Text(Localization.resetButtonTitle.rawValue)
+        .font(.title)
+        .foregroundStyle(.gray)
+    }
+  }
+  
+  private var incrementButton: some View {
+    Button(action: {
+      count += 1
+    }) {
+      Image(systemName: "plus.circle")
+        .font(.largeTitle)
+    }
+  }
+}
+
+// MARK: - Localization
+
+extension CounterView {
+  private enum Localization: String {
+    case resetButtonTitle = "Сброс"
+  }
+}
+
+struct CounterView_Previews: PreviewProvider {
+  static var previews: some View {
+    CounterView()
+  }
 }
